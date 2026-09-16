bad4244235f242510fbeb5e1b066934a1a8fdb65
vipusrihar <vipusrihar@gmail.com>
2026-09-16 19:53:26 +0530

    fix: apiResponse updated

--- src/main/java/com/vipusa/bus/booking/system/response/ApiResponse.java
+++ src/main/java/com/vipusa/bus/booking/system/response/ApiResponse.java
@@ -1,17 +1,31 @@
 package com.vipusa.bus.booking.system.response;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
 @Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Builder
-public class ApiResponse<T>{
+@AllArgsConstructor
+public class ApiResponse<T> {
 
-    private boolean isSuccess;
-
-    private  String message;
-
+    private boolean success;
+    private String message;
     private T response;
+
+    public static <T> ApiResponse<T> success( String message, T response ) {
+        return ApiResponse.<T>builder()
+                .success(true)
+                .message(message)
+                .response(response)
+                .build();
+    }
+
+    public static <T> ApiResponse<T> failure(String message) {
+        return ApiResponse.<T>builder()
+                .success(false)
+                .message(message)
+                .response(null)
+                .build();
+    }
 }
